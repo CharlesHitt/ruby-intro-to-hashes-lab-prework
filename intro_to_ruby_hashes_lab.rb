96036dd2e6efe2117ff4aed41fee9caa7bf92bd8
Charles Hitt <charles.a.hitt@gmail.com>
2019-03-29 00:41:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,11 @@ def monopoly_with_third_tier
 	monopoly = {
 		  :railroads => {
 		    :pieces => 4,
-		    :names => {},
-		    :rent_in_dollars => {}
+		    :names => {
+		    },
+		    :rent_in_dollars => {
+		      :
+		    }
 		  }
 	}
 		
